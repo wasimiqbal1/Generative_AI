49c39ac4e7be0adb20e44caff52d0df4837a047a
wasimiqbal1 <wasim_iqbal@msn.com>
2024-01-14 15:38:48 +0500

    multipages

---
+++ streamlit/multipages/hello.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title = "Hello",
+    page_icon = "👋"
+
+)
+
+st.write("# Welcome's to Zia's Page!")
+
+st.sidebar.success("Click here")
+
---
+++ streamlit/multipages/pages/3_contactus.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.write("Contact us on call & email")
